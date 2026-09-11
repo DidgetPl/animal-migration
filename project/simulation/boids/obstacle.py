8b2fb3edb5a35e33da0b2864997d6b85087d1d82
Robert Barcik <robertbarcik32@gmail.com>
2026-09-11 20:35:36 +0200

    last fixes

--- project/simulation/boids/obstacle.py
+++ project/simulation/boids/obstacle.py
@@ -4,7 +4,7 @@ from mesa import Agent
 class Obstacle(Agent):
     def __init__(self, model, obstacle_type="rock", radius=8.0):
         super().__init__(model)
-        self.obstacle_type = obstacle_type  # "rock", "tree", "bush"
+        self.obstacle_type = obstacle_type
         self.radius = radius
 
     def step(self):
